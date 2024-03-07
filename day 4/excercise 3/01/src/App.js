512fc6008dfd7365b97f5792c8b2e14488d24bce
hashmat1530 <hashmat2020vskp@gmail.com>
2024-03-07 16:00:03 +0530

    day 4 done

--- day 4/excercise 3/01/src/App.js
+++ day 4/excercise 3/01/src/App.js
@@ -1,12 +1,55 @@
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hustler
+export function Img() {
+  return (<>
+    <div className="image">
+      <img src="https://media.licdn.com/dms/image/D5635AQEKtHFuR7wWnQ/profile-framedphoto-shrink_400_400/0/1709732620217?e=1710410400&v=beta&t=RpJHQyLEAZxICOQintypiTPXo1y-LE2bLU-stxwUw1s" alt="" />
     </div>
+    <div className='names'>
+      <h3>Mohammed Hashmatullah☑️</h3>
+      <h6>MERN STACK DEVELOPER, INDIA</h6>
+    </div>
+  </>
   );
 }
 
-export default App;
+export function Skills() {
+  return (<>
+    <div className="skill">
+      <h4>Skills</h4>
+    </div>
+    <div className='skill-set'>
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>SASS</p>
+      <p>JS</p>
+      <p>React</p>
+      <p>Redux</p>
+      <p>Node</p>
+      <p>MongoDB</p>
+      <p>Python</p>
+      <p>Flask</p>
+      <p>Django</p>
+      <p>Numpy</p>
+      <p>Pandas</p>
+      <p>Data Analysis</p>
+      <p>MYSQL</p>
+      <p>GraphQL</p>
+      <p>D3.js</p>
+      <p>Gatsby</p>
+      <p>Docker</p>
+      <p>Heroku</p>
+      <p>Git</p>
+    </div>
+  </>
+  );
+}
+
+
+export function Date() {
+  return <div className='date'>
+    <p>🕧Joined on March 07, 2024</p>
+  </div>
+}
+
